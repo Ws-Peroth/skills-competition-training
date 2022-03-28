f731dd76fcb5955f4e3481e486250cf075f37572
Ws-Peroth <a01033056809@gmail.com>
2022-03-28 21:34:54 +0900

    Add Virus Pattern

--- shoot/Assets/Scripts/Manager/GameManager.cs
+++ shoot/Assets/Scripts/Manager/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -12,7 +13,9 @@ public enum DamageType
 
 public class GameManager : MonoBehaviour
 {
+    [field:SerializeField]
     public float Pain { get; set; }
+    [field:SerializeField]
     public float Hp { get; set; }
     public static GameManager Instance { get; set; }
     
@@ -28,13 +31,16 @@ public class GameManager : MonoBehaviour
     }
 
     public const float BacteriaSpawnY = 5.5f; 
+    public const float VirusSpawnY = 5.5f; 
 
 
     private void Start()
     {
         Pain = 0;
-        Pain = 100;
+        Hp = 100;
+        
         StartCoroutine(BacteriaSpawnRoutine());
+        StartCoroutine(VirusSpawnRoutine());
     }
 
     private IEnumerator BacteriaSpawnRoutine()
@@ -47,8 +53,24 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(Random.Range(1f, 3f));
         }
     }
+    
+    private IEnumerator VirusSpawnRoutine()
+    {
+        while (true)
+        {
+            Debug.Log($"[Virus] Spawn");
+            var mob = PoolManager.Instance.CreatPrefab(PoolCode.Virus);
+            mob.transform.position = new Vector3(Random.Range(-8, 8), VirusSpawnY, 0);
+            ActiveMob(mob);
+            
+            var randomDelay = Random.Range(3f, 6f);
+            Debug.Log($"[Virus] Delay : {randomDelay.ToString()}");
+            yield return new WaitForSeconds(randomDelay);
+        }
+    }
+    
 
-    private void ActiveMob(GameObject mob)
+    private static void ActiveMob(GameObject mob)
     {
         mob.SetActive(true);
         mob.GetComponent<Entity>().InitializeBaseData();
