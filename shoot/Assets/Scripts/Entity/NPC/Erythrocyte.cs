0355862d691e04fbbabef8be93585017e4140194
Ws-Peroth <a01033056809@gmail.com>
2022-03-31 12:39:46 +0900

    Add NPC Script

--- shoot/Assets/Scripts/Entity/NPC/Erythrocyte.cs
+++ shoot/Assets/Scripts/Entity/NPC/Erythrocyte.cs
@@ -1,18 +1,84 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Erythrocyte : MonoBehaviour
+// 적혈구
+public class Erythrocyte : Entity
 {
-    // Start is called before the first frame update
-    void Start()
+    private int _damageStack;
+    
+    public override void InitializeBaseData()
     {
-        
+        _damageStack = 0;
+        Speed = 0.05f;
+        Damage = 10;
+        EntityType = PoolCode.Erythrocyte;
+        IsDestroyed = false;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Move()
     {
+        transform.Translate(Vector3.down * Speed);
+    }
+
+    public override void Damaged(float damage)
+    {
+        return;
+    }
+
+    protected override void Attack()
+    {
+        return;
+    }
+
+    protected override void OnBecameInvisible()
+    {
+        if (IsDestroyed)
+        {
+            // Already Destroy
+            return;
+        }
         
+        // Another Action
+    }
+
+    private void GetPain()
+    {
+        _damageStack++;
+        if (_damageStack > 1)
+        {
+            return;
+        }
+        
+        IsDestroyed = true;
+        GameManager.Instance.Damaged(Damage, DamageType.Pain);
+        
+        PoolManager.Instance.DestroyPrefab(gameObject, EntityType);
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            GetPain();
+        }
+
+        if (col.CompareTag("Enemy"))
+        {
+            GetPain();
+        }
+
+        if (col.CompareTag("Bullet"))
+        {
+            GetPain();
+        }
+
+        base.OnTriggerEnter2D(col);
     }
 }
--- shoot/Assets/Scripts/Entity/NPC/Leukocyte.cs
+++ shoot/Assets/Scripts/Entity/NPC/Leukocyte.cs
@@ -1,18 +1,77 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class Leukocyte : MonoBehaviour
+// 백혈구
+public class Leukocyte : Entity
 {
-    // Start is called before the first frame update
-    void Start()
+    private int _itemCallStack;
+    public override void InitializeBaseData()
     {
-        
+        _itemCallStack = 0;
+        Speed = 0.05f;
+        EntityType = PoolCode.Leukocyte;
+        IsDestroyed = false;
     }
-
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        Move();
+    }
+    protected override void Move()
+    {
+        transform.Translate(Vector3.down * Speed);
+    }
+    public override void Damaged(float damage)
+    {
+        return;
+    }
+    protected override void Attack()
+    {
+        return;
+    }
+    protected override void OnBecameInvisible()
+    {
+        if (IsDestroyed)
+        {
+            // Already Destroy
+            return;
+        }
         
+        // Another Action
+    }
+    private void DropItem()
+    {
+        _itemCallStack++;
+        if (_itemCallStack > 1)
+        {
+            return;
+        }
+        
+        IsDestroyed = true;
+        var randomIndex = Random.Range((int) PoolCode.PowerUpItem, (int) PoolCode.MaxCount);
+        var item = PoolManager.Instance.CreatPrefab((PoolCode) randomIndex);
+        item.GetComponent<Item>().ItemType = (PoolCode) randomIndex;
+        item.transform.position = transform.position;
+        item.SetActive(true);
+
+        // Debug.Log($"[Drop Item] : {(PoolCode) randomIndex} ::: ({_itemCallStack})");
+        PoolManager.Instance.DestroyPrefab(gameObject, PoolCode.Leukocyte);
+    }
+    
+    protected override void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            DropItem();
+        }
+        else if (col.GetComponent<Bullet>() as PlayerBullet)
+        {
+            DropItem();
+        }
+
+        base.OnTriggerEnter2D(col);
     }
 }
